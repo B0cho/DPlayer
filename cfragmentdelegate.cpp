6634b2029dc69d9812404b8ab9cfac8ae1e1b353
Bocho <krzychu96@hotmail.com>
2019-07-10 20:50:24 +0200

    Declaration of paint and sizeHint methods for fragments delegate

--- cfragmentdelegate.cpp
+++ cfragmentdelegate.cpp
@@ -1,6 +1,23 @@
 #include "cfragmentdelegate.h"
 
-CFragmentDelegate::CFragmentDelegate()
+CFragmentDelegate::CFragmentDelegate(QWidget *parent)
+{
+    Q_UNUSED(parent);
+}
+
+void CFragmentDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    if(index.isValid()) return;
+
+    // obtaining data
+    const auto& title = index.data(Qt::DisplayRole).toString();
+    const auto& file = index.data(Qt::ToolTipRole).toString();
+    const auto& scope = index.data(Qt::StatusTipRole).toString();
+    qDebug() << "Painting: " << title;
+
+}
+
+QSize CFragmentDelegate::sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
 
 }
--- cfragmentdelegate.h
+++ cfragmentdelegate.h
@@ -13,8 +13,16 @@
 class CFragmentDelegate : public QStyledItemDelegate
 {
     Q_OBJECT
+private:
+    // default item properties
+    static constexpr QSize itemSize{100, 50};
+    static constexpr QMargins itemMargins{2, 2, 2, 2};
 public:
-    CFragmentDelegate();
+    CFragmentDelegate(QWidget* parent = 0);
+
+    // overriden
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override;
 };
 
 #endif // CFRAGMENTDELEGATE_H
