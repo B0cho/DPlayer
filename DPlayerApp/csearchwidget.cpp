b391f0eebc5011595649286b6118f376aca3908e
Bocho <krzychu96@hotmail.com>
2019-10-12 21:23:59 +0200

    Adding suggestion view size policy

--- DPlayerApp/csearchwidget.cpp
+++ DPlayerApp/csearchwidget.cpp
@@ -6,9 +6,16 @@ CSearchWidget::CSearchWidget(QWidget *parent):
     lineEdit(new QLineEdit()), // edit init
     suggestionView(new QListView()) // list init
 {
+    // setting layout size policy
+    auto sp_retain = suggestionView->sizePolicy();
+    sp_retain.setRetainSizeWhenHidden(true);
+    suggestionView->setSizePolicy(sp_retain);
+    layout->setMargin(0);
+
     // adding widgets to layout
     layout->addWidget(lineEdit.get());
     layout->addWidget(suggestionView.get());
 
-
+    // setting widgets
+    suggestionView->hide();
 }
